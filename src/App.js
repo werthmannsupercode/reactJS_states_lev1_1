d4488449256107707b749bee850afb7bfbd3756d
werthmannsupercode <werthmannsupercode@gmail.com>
2022-06-10 11:59:17 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [answer, setAnswer] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="innerContainer">
+        <p>Why ist React great?</p>
+        <button onClick={() => setAnswer(!answer)}>+</button>
+      </div>
+      <p className="answerP" style={answer ? { display: 'none' } : { display: 'block' }} > Fast Learning Curve</p>
+    </div >
   );
 }
 
